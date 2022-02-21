e419f3c63869570a342910e5c84a9c3dbd36eeea
Frenqy <411937015@qq.com>
2022-02-21 10:26:17 +0800

    Add: 门清自摸和

---
+++ Pattern/门清自摸和.cs
@@ -0,0 +1,20 @@
+﻿using RabiRiichi.Riichi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabiRiichi.Pattern {
+    public class 门清自摸和 : StdPattern {
+        public override Type[] dependOnPatterns => AllBasePatterns;
+
+        public override bool Resolve(List<MenOrJantou> groups, Hand hand, GameTile incoming, Scorings scorings) {
+            if (hand.menzen && incoming.IsTsumo) {
+                scorings.Add(new Scoring(ScoringType.Han, 1, this));
+                return true;
+            }
+            return false;
+        }
+    }
+}
