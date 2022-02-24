b4de15d666833f92fee6b316508c51239f6b4d20
Frenqy <411937015@qq.com>
2022-02-24 16:59:33 +0800

    宝牌一大堆

---
+++ Pattern/宝牌.cs
@@ -0,0 +1,23 @@
+using RabiRiichi.Riichi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RabiRiichi.Pattern {
+    public class 宝牌 : BonusPattern {
+        public override Type[] basePatterns => AllBasePatterns;
+
+        public override bool Resolve(List<MenOrJantou> groups, Hand hand, GameTile incoming, Scorings scorings) {
+            var tiles = groups.SelectMany(tile => tile.ToTiles());
+            int han = 0;
+            foreach (var tile in tiles) {
+                han += hand.game.wall.CountDora(tile);
+            }
+            if (han > 0) {
+                scorings.Add(new Scoring(ScoringType.Han, han, this));
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Pattern/里宝牌.cs
@@ -0,0 +1,26 @@
+using RabiRiichi.Riichi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RabiRiichi.Pattern {
+    public class 里宝牌 : BonusPattern {
+        public override Type[] basePatterns => AllBasePatterns;
+
+        public override bool Resolve(List<MenOrJantou> groups, Hand hand, GameTile incoming, Scorings scorings) {
+            if (!hand.riichi)
+                return false;
+
+            var tiles = groups.SelectMany(tile => tile.ToTiles());
+            int han = 0;
+            foreach (var tile in tiles) {
+                han += hand.game.wall.CountUradora(tile);
+            }
+            if (han > 0) {
+                scorings.Add(new Scoring(ScoringType.Han, han, this));
+                return true;
+            }
+            return false;
+        }
+    }
+}
