23e1423fa74c8b6ef04e1ffb35c192b3457bb5ff
KCFindstr <shimakaze@rabimimi.com>
2022-03-14 00:49:40 +0000

    Add ClaimTileEvent

---
+++ Event/InGame/ClaimTileEvent.cs
@@ -0,0 +1,21 @@
+using RabiRiichi.Interact;
+using RabiRiichi.Riichi;
+
+namespace RabiRiichi.Event.InGame {
+    public class ClaimTileEvent : BroadcastPlayerEvent {
+        public override string name => "claim_tile";
+
+        #region Request
+        [RabiBroadcast] public GameTile tile;
+        [RabiBroadcast] public MenLike group;
+        #endregion
+
+        #region Response
+        #endregion
+
+        public ClaimTileEvent(Game game, int playerId, MenLike group, GameTile tile) : base(game, playerId) {
+            this.group = group;
+            this.tile = tile;
+        }
+    }
+}
