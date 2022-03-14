ef48cedb4ea02e469902e94fae87a571095632ff
Frenqy <411937015@qq.com>
2022-03-14 16:20:53 +0800

    Finish 两番役

---
+++ Pattern/两番役/三色同顺.cs
@@ -0,0 +1,26 @@
+using RabiRiichi.Riichi;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RabiRiichi.Pattern {
+    public class 三色同顺 : StdPattern {
+        public 三色同顺(Base33332 base33332) {
+            BaseOn(base33332);
+        }
+
+        public override bool Resolve(List<MenLike> groups, Hand hand, GameTile incoming, Scorings scorings) {
+            bool is三色同顺 = groups
+                .Where(gr => gr is not Jantou)
+                .OrderBy(gr => gr.First)
+                .Subset(3)
+                .Any(grs => grs.All((gr, index)
+                    => gr is Shun
+                    && (int)gr.Suit == index + 1
+                    && gr.First.tile.Num == grs.First().First.tile.Num));
+            if (is三色同顺) {
+                scorings.Add(new Scoring(ScoringType.Han, hand.menzen ? 2 : 1, this));
+            }
+            return is三色同顺;
+        }
+    }
+}
---
+++ Pattern/两番役/小三元.cs
@@ -0,0 +1,24 @@
+using RabiRiichi.Riichi;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RabiRiichi.Pattern {
+    public class 小三元 : StdPattern {
+        public 小三元(Base33332 base33332) {
+            BaseOn(base33332);
+        }
+
+        public override bool Resolve(List<MenLike> groups, Hand hand, GameTile incoming, Scorings scorings) {
+            bool jantouFlag = groups.Any(gr => gr is Jantou && gr.First.tile.IsSangen);
+            bool kouFlag = groups
+                .Where(gr => gr is not Jantou)
+                .Subset(2)
+                .Any(grs => grs.All(gr => gr.First.tile.IsSangen));
+            if (jantouFlag && kouFlag) {
+                scorings.Add(new Scoring(ScoringType.Han, 2, this));
+                return true;
+            }
+            return false;
+        }
+    }
+}
