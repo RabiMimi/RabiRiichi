36810855ac8e3b042901771b3dd2ad464c7f0ccc
KCFindstr <shimakaze@rabimimi.com>
2022-03-13 22:26:55 -0700

    Implement AgariL:istener

--- Event/InGame/Listener/AgariListener.cs
+++ Event/InGame/Listener/AgariListener.cs
@@ -2,8 +2,13 @@ using System.Threading.Tasks;
 
 namespace RabiRiichi.Event.InGame.Listener {
     public static class AgariListener {
+        public static Task ExecuteAgari(AgariEvent ev) {
+            ev.game.eventBus.Queue(new CalcScoreEvent(ev.game, ev.agariInfos));
+            return Task.CompletedTask;
+        }
+
         public static void Register(EventBus eventBus) {
-            // TODO: Implement
+            eventBus.Register<AgariEvent>(ExecuteAgari, EventPriority.Execute);
         }
     }
 }
