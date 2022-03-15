3cb33a53835ad0c76cf5182b1c40decb504c9aef
Frenqy <411937015@qq.com>
2022-03-15 16:14:27 +0800

    Finish 两杯口

--- Pattern/三番役/两杯口.cs
+++ Pattern/三番役/两杯口.cs
@@ -1,1 +1,29 @@
-// TODO: 两杯口
+using RabiRiichi.Riichi;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RabiRiichi.Pattern {
+    public class 两杯口 : StdPattern {
+        public 两杯口(Base33332 base33332, 一杯口 一杯口) {
+            BaseOn(base33332);
+            DependOn(一杯口);
+        }
+
+        public override bool Resolve(List<MenLike> groups, Hand hand, GameTile incoming, Scorings scorings) {
+            if (!hand.menzen)
+                return false;
+
+            var grs = groups
+                .Where(tiles => tiles is Shun)
+                .GroupBy(gr => gr.Value);
+            bool isValid = grs.Count() == 2 && grs.All(gr => gr.Count() == 2);
+
+            if (isValid) {
+                scorings.Remove(dependOnPatterns);
+                scorings.Add(new Scoring(ScoringType.Han, 3, this));
+                return true;
+            }
+            return false;
+        }
+    }
+}
