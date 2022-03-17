234ded8b8459d8c1de29cc7fc4d3fe2af1fffe91
Frenqy <411937015@qq.com>
2022-03-17 11:08:35 +0800

    岛风：用IsSame

--- Pattern/役满/国士无双十三面.cs
+++ Pattern/役满/国士无双十三面.cs
@@ -10,7 +10,7 @@ namespace RabiRiichi.Pattern {
         }
 
         public override bool Resolve(List<MenLike> groups, Hand hand, GameTile incoming, ScoreStorage scores) {
-            bool flag = hand.freeTiles.Select(tile => tile.tile.Val).Any(val => val == incoming.tile.Val);
+            bool flag = hand.freeTiles.Select(tile => tile.tile).Any(tile => tile.IsSame(incoming.tile));
             if (flag) {
                 scores.Remove(dependOnPatterns);
                 scores.Add(new Scoring(ScoringType.Yakuman, 2, this));
