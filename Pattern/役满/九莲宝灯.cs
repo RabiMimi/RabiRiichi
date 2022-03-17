b9a0771986c77dfca29bb2f34b3184087ce8398a
Frenqy <411937015@qq.com>
2022-03-17 15:37:53 +0800

    Finish 役满

--- Pattern/役满/九莲宝灯.cs
+++ Pattern/役满/九莲宝灯.cs
@@ -1,1 +1,22 @@
-// TODO： 九莲宝灯
+using RabiRiichi.Riichi;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RabiRiichi.Pattern {
+    public class 九莲宝灯 : StdPattern {
+        public 九莲宝灯(Base33332 base33332, 清一色 清一色) {
+            BaseOn(base33332);
+            DependOn(清一色);
+        }
+
+        public override bool Resolve(List<MenLike> groups, Hand hand, GameTile incoming, ScoreStorage scores) {
+            var grs = groups.SelectMany(gr => gr).GroupBy(tile => tile.tile.Num);
+            bool flag = grs.Count() == 9 && grs.First().Count() >= 3 && grs.Last().Count() >= 3;
+            if (flag) {
+                scores.Remove(dependOnPatterns);
+                scores.Add(new Scoring(ScoringType.Yakuman, 1, this));
+            }
+            return flag;
+        }
+    }
+}
--- Pattern/役满/字一色.cs
+++ Pattern/役满/字一色.cs
@@ -1,1 +1,19 @@
-// TODO： 字一色
+using RabiRiichi.Riichi;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RabiRiichi.Pattern {
+    public class 字一色 : StdPattern {
+        public 字一色(AllExcept13_1 allExcept13_1) {
+            BaseOn(allExcept13_1);
+        }
+
+        public override bool Resolve(List<MenLike> groups, Hand hand, GameTile incoming, ScoreStorage scores) {
+            if (groups.SelectMany(gr => gr).All(tile => tile.tile.IsZ)) {
+                scores.Add(new Scoring(ScoringType.Yakuman, 1, this));
+                return true;
+            }
+            return false;
+        }
+    }
+}
