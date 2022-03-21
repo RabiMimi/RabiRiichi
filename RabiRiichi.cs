0ca6492f4024042a8d97d3b6cb63297597689f14
KCFindstr <shimakaze@rabimimi.com>
2022-03-20 21:08:17 -0700

    Add assembly version

--- RabiRiichi.cs
+++ RabiRiichi.cs
@@ -1,5 +1,10 @@
-﻿namespace RabiRiichi {
-    public class RabiRiichi {
-        public RabiRiichi() { }
+﻿using System;
+using System.Reflection;
+
+namespace RabiRiichi {
+    public static class RabiRiichi {
+        public static readonly Lazy<string> VERSION = new(() => {
+            return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        });
     }
 }
