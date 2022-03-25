463ede0030b401aec72f87dd5bf356a55e98ca19
Frenqy <411937015@qq.com>
2022-03-25 17:23:33 +0800

    Finish SuuankouTanki

--- Pattern/Yakuman/SuuankouTanki.cs
+++ Pattern/Yakuman/SuuankouTanki.cs
@@ -1,1 +1,21 @@
-// TODO: do something
+using RabiRiichi.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RabiRiichi.Pattern {
+    public class SuuankouTanki : StdPattern {
+        public SuuankouTanki(Base33332 base33332, Suuankou suuankou) {
+            BaseOn(base33332);
+            DependOn(suuankou);
+        }
+
+        public override bool Resolve(List<MenLike> groups, Hand hand, GameTile incoming, ScoreStorage scores) {
+            if (groups.Find(gr => gr.Contains(incoming)) is Jantou) {
+                scores.Remove(dependOnPatterns);
+                scores.Add(new Scoring(ScoringType.Yakuman, 2, this));
+                return true;
+            }
+            return false;
+        }
+    }
+}
