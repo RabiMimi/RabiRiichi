69aea40babadc90072ad9f1dd43c36c956d56bf9
Frenqy <411937015@qq.com>
2022-03-28 16:30:26 +0800

    Add tests

---
+++ RabiRiichiTests/Pattern/1Han/HouteiRaoyuiTest.cs
@@ -0,0 +1,43 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RabiRiichi.Core;
+using RabiRiichi.Pattern;
+
+namespace RabiRiichiTests.Pattern {
+    [TestClass]
+    public class HouteiRaoyuiTest {
+        protected StdPattern V { get; set; } = new HouteiRaoyui(null);
+
+        [TestMethod]
+        public void TestResolved() {
+            new StdTestBuilder(V)
+                .AddFree("234s")
+                .AddFree("345p")
+                .AddFree("456m")
+                .AddFree("22m")
+                .AddAgari("23s", "4s")
+                .MockWall(wall => wall.SetIsHaitei())
+                .Resolve(true)
+                .ExpectScoring(ScoringType.Han, 1)
+                .NoMore();
+        }
+
+        [TestMethod]
+        public void TestFailed() {
+            new StdTestBuilder(V)
+                .AddFree("234s")
+                .AddFree("345p")
+                .AddFree("456m")
+                .AddFree("22m")
+                .AddAgari("23s", "4s", true)
+                .MockWall(wall => wall.SetIsHaitei())
+                .Resolve(false);
+            new StdTestBuilder(V)
+                .AddFree("234s")
+                .AddFree("345p")
+                .AddFree("456m")
+                .AddFree("22m")
+                .AddAgari("23s", "4s")
+                .Resolve(false);
+        }
+    }
+}
