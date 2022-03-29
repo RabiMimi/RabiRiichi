68e0dd64f34caf32d316033a2d91511547b99e17
KCFindstr <shimakaze@rabimimi.com>
2022-03-29 00:36:13 -0700

    Add AutoIncrementTest

---
+++ RabiRiichiTests/Util/AutoIncrementTest.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RabiRiichi.Util;
+
+namespace RabiRiichiTests.Util {
+    [TestClass]
+    public class AutoIncrementIntTest {
+
+        [TestMethod]
+        public void ConstructAutoIncrementInt() {
+            var i = new AutoIncrementInt(10);
+            int val = i;
+            Assert.AreEqual(10, val);
+            Assert.AreEqual(10, i.Value);
+
+            i = 13;
+            val = i;
+            Assert.AreEqual(13, val);
+            Assert.AreEqual(13, i.Value);
+        }
+
+        [TestMethod]
+        public void ModifyAutoIncrementInt() {
+            var i = new AutoIncrementInt(10);
+            Assert.AreEqual(11, i.Next);
+            Assert.AreEqual(12, i.Next);
+            i.Reset(4);
+            Assert.AreEqual(5, i.Next);
+        }
+    }
+}
