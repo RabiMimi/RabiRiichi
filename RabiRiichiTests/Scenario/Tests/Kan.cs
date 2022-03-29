7b94e85f65c89dbe42c8fe45d5ec727f0cd3567a
Frenqy <411937015@qq.com>
2022-03-29 17:59:35 +0800

    Test Kan

--- RabiRiichiTests/Scenario/Tests/Kan.cs
+++ RabiRiichiTests/Scenario/Tests/Kan.cs
@@ -43,5 +43,65 @@ namespace RabiRiichiTests.Scenario.Tests {
                 CollectionAssert.Contains(game.GetPlayer(0).hand.called, kan);
             });
         }
+
+        [TestMethod]
+        public async Task SuccessAnKan() {
+            var scenario = new ScenarioBuilder()
+                .WithPlayer(0, playerBuilder => {
+                    playerBuilder.SetFreeTiles("11122233345s19m");
+                })
+                .WithWall(wall => wall.Reserve("1s"))
+                .WithWall(wall => wall.AddRinshan("29s"))
+                .Start(0);
+
+            (await scenario.WaitInquiry()).ForPlayer(0, playerInquiry => {
+                playerInquiry
+                    .AssertAction<PlayTileAction>()
+                    .ChooseTiles<KanAction>("1111s", action => {
+                        Assert.AreEqual(1, action.options.Count);
+                        return true;
+                    })
+                    .AssertNoMoreActions();
+            }).AssertAutoFinish();
+
+            Kan kan1 = null;
+            scenario
+                .AssertEvent<KanEvent>((ev) => {
+                    kan1 = ev.kan;
+                    Assert.AreEqual(TileSource.AnKan, ev.kanSource);
+                    return true;
+                })
+                .AssertEvent<RevealDoraEvent>(ev => {
+                    Assert.AreEqual(0, ev.playerId);
+                    return true;
+                });
+
+            (await scenario.WaitInquiry()).ForPlayer(0, playerInquiry => {
+                playerInquiry
+                    .AssertAction<PlayTileAction>()
+                    .ChooseTiles<KanAction>("2222s", action => {
+                        Assert.AreEqual(1, action.options.Count);
+                        return true;
+                    })
+                    .AssertNoMoreActions();
+            }).AssertAutoFinish();
+
+            Kan kan2 = null;
+            scenario
+                .AssertEvent<AddKanEvent>((ev) => {
+                    kan2 = ev.kan;
+                    Assert.AreEqual(TileSource.AnKan, ev.kanSource);
+                    return true;
+                })
+                .AssertEvent<RevealDoraEvent>(ev => {
+                    Assert.AreEqual(0, ev.playerId);
+                    return true;
+                });
+
+            // scenario.WithGame(game => {
+            //     CollectionAssert.Contains(game.GetPlayer(0).hand.freeTiles, kan1);
+            //     CollectionAssert.Contains(game.GetPlayer(0).hand.freeTiles, kan2);
+            // });
+        }
     }
 }
