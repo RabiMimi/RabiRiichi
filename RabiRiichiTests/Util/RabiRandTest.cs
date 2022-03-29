77eb48cc8a3547f5282fba961c268c98fc740ba1
KCFindstr <shimakaze@rabimimi.com>
2022-03-29 00:23:41 -0700

    Add RabiRandTests

--- RabiRiichiTests/Util/RabiRandTest.cs
+++ RabiRiichiTests/Util/RabiRandTest.cs
@@ -1,16 +1,24 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RabiRiichi.Util;
 using System.Collections.Generic;
+using System.Linq;
+
 
 namespace RabiRiichiTests.Util {
     [TestClass]
     public class RabiRandTest {
+        private RabiRand rand;
+
+        [TestInitialize]
+        public void Setup() {
+            rand = new RabiRand(114514);
+        }
+
         [TestMethod]
         public void TestRabiRandDistribution() {
             int randCount = 1000000;
             int bucketCount = 100;
             float errRate = 0.1f;
-            var rand = new RabiRand(114514);
             var histogram = new int[bucketCount];
             for (var i = 0; i < randCount; i++) {
                 histogram[rand.Next(bucketCount)]++;
@@ -27,14 +35,47 @@ namespace RabiRiichiTests.Util {
         [TestMethod]
         public void TestRabiRandPeriod() {
             int randCount = 1000000;
-            ulong seed = 114514;
-            HashSet<ulong> seedSet = new() { seed };
-            var rand = new RabiRand(seed);
+            HashSet<ulong> seedSet = new() { rand.seed };
             for (var i = 0; i < randCount; i++) {
-                seed = rand.Next();
+                var seed = rand.Next();
                 Assert.IsFalse(seedSet.Contains(seed), $"seed {seed} is duplicated");
                 seedSet.Add(seed);
             }
+        }
+
+        [TestMethod]
+        public void TestNext() {
+            int randCount = 1000;
+            for (int i = 0; i < randCount; i++) {
+                Assert.AreEqual(i, rand.Next(i, i + 1));
+                var next3 = rand.Next(3);
+                Assert.IsTrue(next3 >= 0 && next3 < 3);
+                next3 = rand.Next(i, i + 3);
+                Assert.IsTrue(next3 >= i && next3 < i + 3);
+            }
+        }
+
+        [TestMethod]
+        public void TestShuffle() {
+            var list = new List<int>() { 10, 1, 1, 2, 2, 2, 3, 4, 9, 6, 6 };
+            var toShuffle = list.ToList();
+            rand.Shuffle(toShuffle);
+            CollectionAssert.AreNotEqual(list, toShuffle);
+            CollectionAssert.AreEquivalent(list, toShuffle);
+        }
+
+        [TestMethod]
+        public void TestChoice() {
+            var list = new List<int>() { 10, 1, 1, 2, 2, 2, 3, 4, 9, 6, 6 };
+            CollectionAssert.Contains(list, rand.Choice(list));
+            var choices = rand.Choice(list, 3).ToArray();
+            foreach (var choice in choices) {
+                CollectionAssert.Contains(list, choice);
+            }
+            Assert.AreEqual(3, choices.Length);
+            choices = rand.Choice(list, list.Count).ToArray();
+            CollectionAssert.AreNotEqual(list, choices);
+            CollectionAssert.AreEquivalent(list, choices);
         }
     }
 }
