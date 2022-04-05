76c94d7400baa53421ad462765ef11e8084251d4
KCFindstr <shimakaze@rabimimi.com>
2022-04-04 21:24:07 -0700

    Add Suukantsu scenario test

--- RabiRiichiTests/Scenario/Tests/Kan.cs
+++ RabiRiichiTests/Scenario/Tests/Kan.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RabiRiichi.Action;
 using RabiRiichi.Core;
 using RabiRiichi.Event.InGame;
+using RabiRiichi.Pattern;
 using RabiRiichiTests.Helper;
 using System.Threading.Tasks;
 
@@ -15,7 +16,7 @@ namespace RabiRiichiTests.Scenario.Tests {
                 .WithPlayer(0, playerBuilder => {
                     playerBuilder.SetFreeTiles("11112223333s19m");
                 })
-                .WithWall(wall => wall.Reserve("2s"))
+                .WithWall(wall => wall.Reserve("2s").AddRinshan("1119m"))
                 .Start(1);
 
             (await scenario.WaitInquiry()).ForPlayer(1, playerInquiry => {
@@ -54,6 +55,7 @@ namespace RabiRiichiTests.Scenario.Tests {
                 return true;
             });
 
+            // AnKan after AnKan
             (await scenario.WaitInquiry()).ForPlayer(0, playerInquiry => {
                 playerInquiry
                     .AssertAction<PlayTileAction>()
@@ -64,17 +66,50 @@ namespace RabiRiichiTests.Scenario.Tests {
                     .AssertNoMoreActions();
             }).AssertAutoFinish();
 
-            await scenario.AssertEvent<AddKanEvent>((ev) => {
+            scenario.AssertEvent<AddKanEvent>((ev) => {
                 Assert.AreEqual(TileSource.AnKan, ev.kanSource);
                 return true;
-            }).Resolve();
+            });
+
+            // 4th AnKan
+            (await scenario.WaitInquiry()).ForPlayer(0, playerInquiry => {
+                playerInquiry
+                    .AssertAction<PlayTileAction>()
+                    .ChooseTiles<KanAction>("1111m", action => {
+                        Assert.AreEqual(1, action.options.Count);
+                        return true;
+                    })
+                    .AssertNoMoreActions();
+            }).AssertAutoFinish();
+
+            var inquiry = await scenario.AssertEvent<AddKanEvent>((ev) => {
+                Assert.AreEqual(TileSource.AnKan, ev.kanSource);
+                return true;
+            }).WaitInquiry();
 
             // Check Kans exist
             scenario.WithPlayer(0, player => {
                 player.hand.called.AssertContains("1111s");
                 player.hand.called.AssertContains("2222s");
                 player.hand.called.AssertContains("3333s");
+                player.hand.called.AssertContains("1111m");
             });
+
+            // Suukantsu
+            inquiry.ForPlayer(0, playerInquiry => {
+                playerInquiry
+                    .AssertAction<PlayTileAction>()
+                    .ApplyAction<TsumoAction>()
+                    .AssertNoMoreActions();
+            }).AssertAutoFinish();
+
+            await scenario.AssertEvent<AgariEvent>((ev) => {
+                ev.agariInfos
+                    .AssertTsumo(0)
+                    .AssertScore(yakuman: 1)
+                    .AssertYaku<Suukantsu>(yakuman: 1);
+                return true;
+            }).Resolve();
         }
 
         [TestMethod]
