3a2504bee03eea04c9d264f37befd507ae14410b
KCFindstr <shimakaze@rabimimi.com>
2022-06-05 23:19:19 -0700

    Finish Fuuro tests

--- RabiRiichiTests/Scenario/Tests/Fuuro.cs
+++ RabiRiichiTests/Scenario/Tests/Fuuro.cs
@@ -1,7 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RabiRiichi.Action;
+using RabiRiichi.Core;
 using RabiRiichi.Core.Config;
 using RabiRiichi.Event.InGame;
+using RabiRiichi.Pattern;
 using RabiRiichiTests.Helper;
 using System;
 using System.Linq;
@@ -53,7 +55,7 @@ namespace RabiRiichiTests.Scenario.Tests {
             });
         }
 
-        private async Task<Scenario> MultiChoiceChiiScenario(string expectedDiscardTiles, Action<ScenarioBuilder> setup = null) {
+        private static async Task<Scenario> MultiChoiceChiiScenario(string expectedDiscardTiles, Action<ScenarioBuilder> setup = null) {
             var scenarioBuilder = new ScenarioBuilder()
                 .WithPlayer(2, playerBuilder => playerBuilder
                     .SetFreeTiles("34r55678m227999p"))
@@ -139,6 +141,149 @@ namespace RabiRiichiTests.Scenario.Tests {
 
             // No action for player 2
             await scenario.AssertEvent<NextPlayerEvent>().Resolve();
+        }
+        #endregion
+
+        #region Pon
+        [TestMethod]
+        public async Task SimplePon_CannotRiichi() {
+            var scenario = new ScenarioBuilder()
+                .WithPlayer(2, playerBuilder => playerBuilder
+                    .SetFreeTiles("99m11122357999p"))
+                .WithWall(wall => wall.Reserve("9m"))
+                .Start(3);
+
+            (await scenario.WaitInquiry()).Finish();
+
+            scenario.WithPlayer(2, player => Assert.IsTrue(player.hand.menzen));
+
+            (await scenario.WaitInquiry()).ForPlayer(2, playerInquiry => playerInquiry
+                .AssertSkip()
+                .ChooseTiles<PonAction>("999m", action => {
+                    Assert.AreEqual(1, action.options.Count);
+                    return true;
+                })
+                .AssertNoMoreActions()
+            ).AssertAutoFinish();
+
+            scenario.AssertNoEvent<DrawTileEvent>();
+
+            (await scenario.WaitInquiry()).ForPlayer(2, playerInquiry => {
+                playerInquiry
+                    .ChooseTile<PlayTileAction>("3p", action => {
+                        action.options.ToTiles().AssertEquals("11122357999p");
+                        return true;
+                    })
+                    .AssertNoMoreActions();
+            }).AssertAutoFinish();
+
+            await scenario.WithPlayer(2, player => {
+                Assert.IsFalse(player.hand.menzen);
+                Assert.IsTrue(player.hand.called.ToStrings().SequenceEqual(new string[] { "999m" }));
+            }).AssertEvent<DrawTileEvent>(ev => {
+                Assert.AreEqual(3, ev.playerId);
+                return true;
+            }).Resolve();
+
+            (await scenario.WaitPlayerTurn(2)).ForPlayer(2, playerInquiry => {
+                playerInquiry.AssertNoAction<RiichiAction>();
+            });
+        }
+
+        private static async Task<Scenario> MultiChoicePonScenario(string expectedDiscardTiles, Action<ScenarioBuilder> setup = null) {
+            var scenarioBuilder = new ScenarioBuilder()
+                .WithPlayer(2, playerBuilder => playerBuilder
+                    .SetFreeTiles("555r5678m227999p"));
+            setup?.Invoke(scenarioBuilder);
+            var scenario = scenarioBuilder.Build(0);
+            scenario.WithWall(wall => wall.remaining[^1].tile = new Tile("5m")).Start();
+
+            (await scenario.WaitInquiry()).Finish();
+
+            scenario.WithPlayer(2, player => Assert.IsTrue(player.hand.menzen));
+
+            (await scenario.WaitInquiry()).ForPlayer(2, playerInquiry => playerInquiry
+                .AssertSkip()
+                .AssertAction<KanAction>()
+                .ChooseTiles<PonAction>("555m", action => {
+                    action.options.ToStrings().SequenceEqualAfterSort(
+                        new string[] { "555m", "55r5m" }
+                    );
+                    return true;
+                })
+                .AssertNoMoreActions()
+            ).AssertAutoFinish();
+
+            scenario.AssertNoEvent<DrawTileEvent>();
+
+            (await scenario.WaitInquiry()).ForPlayer(2, playerInquiry => {
+                playerInquiry
+                    .AssertAction<PlayTileAction>(action => {
+                        action.options.ToTiles().AssertEquals(expectedDiscardTiles);
+                        return true;
+                    })
+                    .AssertNoMoreActions();
+            });
+
+            return scenario;
+        }
+
+        [TestMethod]
+        public Task Pon_MultiChoice() {
+            return MultiChoicePonScenario("678m227999p");
+        }
+
+        [TestMethod]
+        public Task Pon_NoGenbutsuKuikae() {
+            return MultiChoicePonScenario("5r5678m227999p", scenarioBuilder => {
+                scenarioBuilder.WithConfig(config =>
+                    config.SetKuikaePolicy(KuikaePolicy.Suji));
+            });
+        }
+
+        [TestMethod]
+        public async Task FailPon_DiscardBySelf() {
+            var scenario = new ScenarioBuilder()
+                .WithPlayer(2, playerBuilder => playerBuilder
+                    .SetFreeTiles("99m11122357999p"))
+                .WithWall(wall => wall.Reserve("9m"))
+                .Start(2);
+
+            (await scenario.WaitInquiry()).Finish();
+
+            (await scenario.WaitInquiry()).AssertNoActionForPlayer(2);
+        }
+
+        [TestMethod]
+        public async Task FailPon_NoTileToPlay() {
+            var scenario = new ScenarioBuilder()
+                .WithPlayer(2, playerBuilder => playerBuilder
+                    .SetFreeTiles("555r5m")
+                    .AddCalled("111m", 0, 3)
+                    .AddCalled("999m", 0, 3)
+                    .AddCalled("111p", 0, 3))
+                .Build(0)
+                .WithWall(wall => wall.remaining[^1].tile = new Tile("5m"))
+                .Start();
+
+            (await scenario.WaitInquiry()).Finish();
+
+            // Player 2 cannot pon
+            (await scenario.WaitInquiry()).ForPlayer(2, playerInquiry => {
+                playerInquiry
+                    .AssertSkip()
+                    .ApplyAction<RonAction>()
+                    .AssertNoMoreActions();
+            });
+
+            await scenario.AssertEvent<AgariEvent>(ev => {
+                ev.agariInfos
+                    .AssertRon(0, 2)
+                    .AssertScore(han: 3)
+                    .AssertYaku<Toitoi>()
+                    .AssertYaku<Akadora>(han: 1);
+                return true;
+            }).Resolve();
         }
         #endregion
     }
