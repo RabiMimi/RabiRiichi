02e60e6f7ebd1989e8435151e059076155fb7ed7
KCFindstr <shimakaze@rabimimi.com>
2022-06-05 23:24:32 -0700

    Add yaku: ShiiaruRaotai

---
+++ RabiRiichiTests/Pattern/1Han/ShiiaruRaotaiTest.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RabiRiichi.Pattern;
+
+namespace RabiRiichiTests.Pattern {
+    [TestClass]
+    public class ShiiaruRaotaiTest {
+        protected StdPattern V { get; set; } = new ShiiaruRaotai(null);
+
+        [TestMethod]
+        public void TestResolved() {
+            new StdTestBuilder(V)
+                .AddCalled("234s", 0)
+                .AddCalled("333p", 1)
+                .AddCalled("789m", 2)
+                .AddCalled("2222m", 3)
+                .AddAgari("2s", "2s", true)
+                .Resolve(true)
+                .ExpectScoring(ScoringType.Han, 1)
+                .NoMore();
+
+            new StdTestBuilder(V)
+                .AddCalled("234s", 0)
+                .AddCalled("333p", 1)
+                .AddCalled("789m", 2)
+                .AddCalled("2222m", 3)
+                .AddAgari("2s", "2s", false)
+                .Resolve(true)
+                .ExpectScoring(ScoringType.Han, 1)
+                .NoMore();
+        }
+
+        [TestMethod]
+        public void TestFailed() {
+            new StdTestBuilder(V)
+                .AddCalled("234s", 0)
+                .AddCalled("333p", 1)
+                .AddFree("789m")
+                .AddCalled("2222m", 2)
+                .AddAgari("2s", "2s", true)
+                .Resolve(false);
+
+            new StdTestBuilder(V)
+                .AddFree("234s")
+                .AddCalled("333p", 1)
+                .AddCalled("789m", 0)
+                .AddCalled("2222m", 2)
+                .AddAgari("2s", "2s", false)
+                .Resolve(false);
+        }
+    }
+}
