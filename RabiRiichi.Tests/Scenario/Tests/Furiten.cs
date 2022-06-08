e864e31514e3f509483a6a72549513afe044a401
KCFindstr <shimakaze@rabimimi.com>
2022-06-08 14:43:49 -0700

    Finish Furiten scenario tests

--- RabiRiichi.Tests/Scenario/Tests/Furiten.cs
+++ RabiRiichi.Tests/Scenario/Tests/Furiten.cs
@@ -7,6 +7,58 @@ using System.Threading.Tasks;
 namespace RabiRiichi.Tests.Scenario.Tests {
     [TestClass]
     public class ScenarioFuriten {
+        #region General
+        [TestMethod]
+        public async Task Furiten_ClearedBetweenRounds() {
+            var scenario = new ScenarioBuilder()
+                .WithPlayer(1, player => player
+                    .SetFreeTiles("1112345678999s"))
+                .WithPlayer(0, player => player
+                    .SetFreeTiles("3334445666777s")
+                    .SetRiichiTile("1p"))
+                .WithWall(wall => wall.Reserve("r5s1p"))
+                .Start(1);
+
+            (await scenario.WaitInquiry()).ForPlayer(1, playerInquiry => {
+                scenario.ForceHaitei();
+                playerInquiry.ChooseTile<PlayTileAction>("r5s");
+            }).AssertAutoFinish();
+
+            (await scenario.WaitInquiry()).ForPlayer(0, playerInquiry => {
+                playerInquiry.ApplySkip();
+            }).AssertAutoFinish();
+
+            var inquiry = await scenario.WaitInquiry();
+
+            scenario.WithPlayer(1, player => {
+                Assert.IsTrue(player.hand.isFuriten);
+                Assert.IsFalse(player.hand.isRiichiFuriten);
+                Assert.IsTrue(player.hand.isDiscardFuriten);
+                Assert.IsFalse(player.hand.isTempFuriten);
+            });
+
+            scenario.WithPlayer(0, player => {
+                Assert.IsTrue(player.hand.isFuriten);
+                Assert.IsTrue(player.hand.isRiichiFuriten);
+                Assert.IsFalse(player.hand.isDiscardFuriten);
+                Assert.IsTrue(player.hand.isTempFuriten);
+            });
+
+            inquiry.Finish();
+
+            inquiry = await scenario.WaitInquiry();
+
+            for (int i = 0; i < 4; i++) {
+                scenario.WithPlayer(i, player => {
+                    Assert.IsFalse(player.hand.isFuriten);
+                    Assert.IsFalse(player.hand.isRiichiFuriten);
+                    Assert.IsFalse(player.hand.isDiscardFuriten);
+                    Assert.IsFalse(player.hand.isTempFuriten);
+                });
+            }
+        }
+        #endregion
+
         #region Temporary Furiten
         [TestMethod]
         public async Task TemporaryFuriten() {
@@ -15,24 +67,24 @@ namespace RabiRiichi.Tests.Scenario.Tests {
                     player.SetFreeTiles("123456789m11z34s");
                 })
                 .WithWall(wall => {
-                    wall.Reserve("5m2s2s2s5m");
+                    wall.Reserve("2sr5s2s5m");
                 })
-                .Start(1);
+                .Start(2);
 
-            (await scenario.WaitInquiry()).Finish(); // 玩家1摸切
+            var inquiry = await scenario.WaitInquiry();
 
             scenario.WithPlayer(1, player => {
                 Assert.IsFalse(player.hand.isFuriten);
                 Assert.IsFalse(player.hand.isTempFuriten);
             });
 
-            (await scenario.WaitInquiry()).Finish(); // 玩家2摸切
+            inquiry.Finish(); // 玩家2摸切
 
             (await scenario.WaitInquiry()).ForPlayer(1, playerInquiry => {
                 playerInquiry.ApplySkip();
             }).AssertAutoFinish();  // 玩家1见逃
 
-            var inquiry = await scenario.WaitInquiry(); // 玩家3回合
+            inquiry = await scenario.WaitInquiry(); // 玩家3回合
 
             // Enters Temporary Furiten after refusing to Ron a valid discarded tile
             scenario.WithPlayer(1, player => {
@@ -101,6 +153,182 @@ namespace RabiRiichi.Tests.Scenario.Tests {
                 .AssertScore(han: 1)
                 .AssertYaku<Tanyao>()).Resolve();
         }
+
+        [TestMethod]
+        public async Task TemporaryFuriten_ClaimingTiles() {
+            var scenario = new ScenarioBuilder()
+                .WithPlayer(1, player => {
+                    player.SetFreeTiles("123456789m11z34s");
+                })
+                .WithPlayer(2, player => {
+                    player.SetFreeTiles("123456789m55s12z");
+                })
+                .WithWall(wall => {
+                    wall.Reserve("2sr5s2s5m");
+                })
+                .Start(2);
+
+            var inquiry = await scenario.WaitInquiry();
+
+            scenario.WithPlayer(1, player => {
+                Assert.IsFalse(player.hand.isFuriten);
+                Assert.IsFalse(player.hand.isTempFuriten);
+            });
+
+            inquiry.Finish(); // 玩家2摸切
+
+            (await scenario.WaitInquiry()).ForPlayer(1, playerInquiry => {
+                playerInquiry.ApplySkip();
+            }).AssertAutoFinish();  // 玩家1见逃
+
+            inquiry = await scenario.WaitInquiry(); // 玩家3回合
+
+            scenario.WithPlayer(1, player => {
+                Assert.IsTrue(player.hand.isFuriten);
+                Assert.IsTrue(player.hand.isTempFuriten);
+            });
+
+            inquiry.Finish(); // 玩家3摸切
+
+            (await scenario.WaitInquiry())
+                .AssertNoActionForPlayer(1)
+                .ForPlayer(2, playerInquiry => {
+                    playerInquiry.ChooseTiles<PonAction>("55r5s");
+                })
+                .AssertAutoFinish(); // 玩家2碰
+
+            inquiry = await scenario.WaitInquiry();
+
+            // 玩家1依然振听
+            scenario.WithPlayer(1, player => {
+                Assert.IsTrue(player.hand.isFuriten);
+                Assert.IsTrue(player.hand.isTempFuriten);
+            });
+
+            // 玩家2切牌
+            inquiry.ForPlayer(2, playerInquiry => playerInquiry
+                .ChooseTile<PlayTileAction>("1z")).AssertAutoFinish();
+
+            // 玩家1碰
+            (await scenario.WaitInquiry()).ForPlayer(1, playerInquiry => {
+                playerInquiry.ChooseTiles<PonAction>("111z");
+            }).AssertAutoFinish();
+
+            await scenario.WaitInquiry();
+
+            scenario.WithPlayer(1, player => {
+                Assert.IsFalse(player.hand.isFuriten);
+                Assert.IsFalse(player.hand.isTempFuriten);
+            });
+        }
+
+        #endregion
+
+        #region Discard Furiten
+
+        [TestMethod]
+        public async Task DiscardFuriten() {
+            var scenario = new ScenarioBuilder()
+                .WithPlayer(1, player => {
+                    player.SetFreeTiles("123456789m11z34s");
+                })
+                .WithWall(wall => {
+                    wall.Reserve("r52593s");
+                })
+                .Start(1);
+
+            var inquiry = await scenario.WaitInquiry();
+
+            scenario.WithPlayer(1, player => {
+                Assert.IsFalse(player.hand.isFuriten);
+                Assert.IsFalse(player.hand.isDiscardFuriten);
+            });
+
+            inquiry.ForPlayer(1, playerInquiry => {
+                playerInquiry.ChooseTile<PlayTileAction>("r5s");
+            }).AssertAutoFinish(); // 玩家1摸切
+
+            for (int i = 0; i < 3; i++) {
+                // 其余三个玩家摸切，玩家1无法荣和
+                inquiry = await scenario.WaitInquiry();
+
+                scenario.WithPlayer(1, player => {
+                    Assert.IsTrue(player.hand.isFuriten);
+                    Assert.IsTrue(player.hand.isDiscardFuriten);
+                });
+
+                inquiry.AssertNoActionForPlayer(1).Finish();
+            }
+
+            (await scenario.WaitInquiry()).ForPlayer(1, playerInquiry => {
+                playerInquiry.ChooseTile<PlayTileAction>("4s");
+            }).AssertAutoFinish();
+
+            inquiry = await scenario.WaitInquiry();
+
+            // 不再舍牌振听
+            scenario.WithPlayer(1, player => {
+                Assert.IsFalse(player.hand.isFuriten);
+                Assert.IsFalse(player.hand.isDiscardFuriten);
+            });
+        }
+
+        [TestMethod]
+        public async Task DiscardFuriten_AfterTenpaiChanges() {
+            var scenario = new ScenarioBuilder()
+                .WithPlayer(1, player => {
+                    player.SetFreeTiles("1112345678999s");
+                })
+                .WithWall(wall => {
+                    wall.Reserve("r52593s");
+                })
+                .Start(1);
+
+            var inquiry = await scenario.WaitInquiry();
+
+            scenario.WithPlayer(1, player => {
+                Assert.IsFalse(player.hand.isFuriten);
+                Assert.IsFalse(player.hand.isDiscardFuriten);
+            });
+
+            inquiry.ForPlayer(1, playerInquiry => {
+                playerInquiry.ChooseTile<PlayTileAction>("r5s");
+            }).AssertAutoFinish(); // 玩家1摸切
+
+            for (int i = 0; i < 3; i++) {
+                // 其余三个玩家摸切，玩家1无法荣和
+                inquiry = await scenario.WaitInquiry();
+
+                scenario.WithPlayer(1, player => {
+                    Assert.IsTrue(player.hand.isFuriten);
+                    Assert.IsTrue(player.hand.isDiscardFuriten);
+                });
+
+                inquiry.AssertNoActionForPlayer(1).Finish();
+            }
+
+            (await scenario.WaitInquiry()).ForPlayer(1, playerInquiry => {
+                playerInquiry
+                    .AssertAction<ChiiAction>()
+                    .AssertAction<KanAction>()
+                    .AssertAction<PonAction>()
+                    .ApplySkip()
+                    .AssertNoMoreActions();
+            }).AssertAutoFinish();
+
+            (await scenario.WaitInquiry()).ForPlayer(1, playerInquiry => {
+                playerInquiry.ChooseTile<PlayTileAction>("2s");
+            }).AssertAutoFinish();
+
+            inquiry = await scenario.WaitInquiry();
+
+            // 依然舍牌振听
+            scenario.WithPlayer(1, player => {
+                Assert.IsTrue(player.hand.isFuriten);
+                Assert.IsTrue(player.hand.isDiscardFuriten);
+            });
+        }
+
         #endregion
     }
 }
