d3dc90d3425179d5dc0e8247f03a0bf3fee3219a
KCFindstr <shimakaze@rabimimi.com>
2022-06-09 23:33:27 -0700

    Add RabiWSContext

--- RabiRiichi.Server/Core/Connection.cs
+++ RabiRiichi.Server/Core/Connection.cs
@@ -9,8 +9,8 @@ using System.Text.Json.Serialization;
 
 namespace RabiRiichi.Server.Core {
     public class Connection : IDisposable {
-        protected class Message {
-            [JsonIgnore] public AtomicBool isQueued;
+        internal class Message {
+            [JsonIgnore] public readonly AtomicBool isQueued = new();
             public bool TryQueue(BlockingCollection<Message> queue) {
                 if (isQueued.Exchange(true)) {
                     return false;
@@ -23,11 +23,23 @@ namespace RabiRiichi.Server.Core {
             public object message;
         }
 
+        protected class HeartBeatMessage {
+            public string msgType => "hb";
+            public int evId { get; init; }
+
+            public HeartBeatMessage(int evId) {
+                this.evId = evId;
+            }
+        }
+
+        protected RabiWSContext currentCtx;
+
         /// <summary>
-        /// Message queue (unique for each websocket)
+        /// Switch to a new WS context. Old connection will be cancelled.
         /// </summary>
-        protected BlockingCollection<Message> msgQueue;
-        protected CancellationTokenSource sendCts;
+        protected void SwitchWSContext(RabiWSContext newCts) {
+            Interlocked.Exchange(ref currentCtx, newCts)?.cts.Cancel();
+        }
 
         protected readonly User user;
         protected readonly int playerId;
@@ -38,16 +50,8 @@ namespace RabiRiichi.Server.Core {
         /// <summary>
         /// Mapping from message ID to message. Shared by all connections with the same player.
         /// </summary>
-        protected readonly ConcurrentDictionary<int, Message> msgLookup = new();
+        internal readonly ConcurrentDictionary<int, Message> msgLookup = new();
 
-        /// <summary>
-        /// Callback when a message is received.
-        /// </summary>
-        public Action<WebSocket, JsonElement> OnReceive;
-        /// <summary>
-        /// Callback when the player is disconnected.
-        /// </summary>
-        public Action<WebSocket> OnDisconnect;
         /// <summary>
         /// Whether the connection is closed.<br/>
         /// If true, no more web sockets or connections are accepted.
@@ -60,55 +64,16 @@ namespace RabiRiichi.Server.Core {
         }
 
         #region Send message
-        protected async Task SendMsgLoop(WebSocket ws) {
-            var queue = msgQueue;
-            var cts = sendCts;
-            while (true) {
-                try {
-                    var msg = queue.Take(cts.Token);
-                    msg.isQueued.Set(false);
-                    var jsonStr = RabiJson.Stringify(msg, playerId);
-                    await ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(jsonStr)),
-                        WebSocketMessageType.Text, true, cts.Token);
-                } catch (OperationCanceledException) {
-                    // Cancellation token cancelled. Could be:
-                    // 1. A new ws connection coming in
-                    // 2. Client informed that they closed the connection
-                    break;
-                } catch (InvalidOperationException) {
-                    // Cannot take from queue anymore.
-                    // The upper layer has specified that no more message is being sent,
-                    // so we can safely close the connection. No further connection will be made.
-                    break;
-                }
-            }
-            // Closed connection
-            await ws.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
-            // Notify receive loop to stop
-            if (Interlocked.CompareExchange(ref sendCts, null, cts) == cts) {
-                cts.Cancel();
-                cts.Dispose();
-            }
-            // Invoke callback. This will be called once and only once.
-            OnDisconnect?.Invoke(ws);
-        }
-
-        protected bool Requeue(int id) {
-            if (msgLookup.TryGetValue(id, out var msg)) {
-                return msg.TryQueue(msgQueue);
-            }
-            return false;
-        }
 
         /// <summary>
         /// Add a message to queue. It will not be sent immediately.
         /// </summary>
-        public void Queue(object msg) {
+        public void Queue<T>(T msg) {
             if (isClosed) {
-                throw new InvalidOperationException("Connection is closed");
+                return;
             }
             try {
-                msgQueue.Add(new Message {
+                currentCtx?.Queue(new Message {
                     id = lastMsgId.Next,
                     message = msg,
                 });
@@ -120,67 +85,67 @@ namespace RabiRiichi.Server.Core {
         /// <summary>
         /// Connects to a websocket. Existing connection will be closed.
         /// </summary>
-        public void Connect(WebSocket ws) {
+        public RabiWSContext Connect(WebSocket ws) {
             if (isClosed) {
                 throw new InvalidOperationException("Connection is closed");
             }
-            // Cancel previous connection
-            var oldCts = Interlocked.Exchange(ref sendCts, new CancellationTokenSource());
-            if (oldCts != null) {
-                oldCts.Cancel();
-                oldCts.Dispose();
-            }
 
-            // Use new message queue and message loops
-            msgQueue = new BlockingCollection<Message>();
-            Task.Run(() => SendMsgLoop(ws));
-            Task.Run(() => ReceiveMsgLoop(ws));
+            var ctx = new RabiWSContext(ws, playerId);
+            // Start message loops before context switch
+            ctx.RunLoops(Task.Run(() => HeartBeatLoop(ctx))).ConfigureAwait(false);
+
+            // Cancel previous connection and switch context
+            SwitchWSContext(ctx);
+
+            return ctx;
         }
         #endregion
 
         #region Receive Msg
 
-        protected async Task ReceiveMsgLoop(WebSocket ws) {
-            var cts = sendCts;
-            var byteArr = new ArraySegment<byte>(new byte[1024]);
-            while (true) {
-                try {
-                    var msg = await ws.ReceiveAsync(byteArr, cts.Token);
-                    if (!msg.EndOfMessage) {
-                        // Too long, refuse to parse
-                        while (!(await ws.ReceiveAsync(byteArr, cts.Token)).EndOfMessage) { }
-                        continue;
-                    }
-                    if (msg.MessageType == WebSocketMessageType.Close) {
-                        // Client closes connection, notify send loop to close
-                        break;
-                    } else if (msg.MessageType == WebSocketMessageType.Text) {
-                        var jsonStr = Encoding.UTF8.GetString(byteArr.Array, 0, msg.Count);
-                        var json = JsonSerializer.Deserialize<JsonElement>(jsonStr);
-                        OnReceive?.Invoke(ws, json);
-                    }
-                } catch (OperationCanceledException) {
-                    // Cancellation token cancelled. Could be:
-                    // 1. A new ws connection coming in
-                    // 2. Client informed that they closed the connection
-                    return;
-                } catch (ArgumentException) {
-                    // Invalid message
-                    continue;
-                } catch (JsonException) {
-                    // Invalid message
-                    continue;
-                }
-            }
-            // Client closed connection, notify send loop to close
-            if (Interlocked.CompareExchange(ref sendCts, null, cts) == cts) {
-                cts.Cancel();
-                cts.Dispose();
-            }
-        }
         #endregion
 
         #region Disconnect & Dispose
+        private void HeartBeatLoop(RabiWSContext ctx) {
+            var received = new ManualResetEvent(false);
+            ctx.OnReceive += (JsonElement json) => {
+                received.Set();
+                try {
+                    // Client sends heart beat requesting an event to be resent.
+                    if (json.TryGetProperty("msgType", out var msgType)
+                        && msgType.GetString() == "hb"
+                        && json.TryGetProperty("evId", out var evId)
+                        && evId.TryGetInt32(out var evIdInt)
+                        && evIdInt > 0
+                        && msgLookup.TryGetValue(evIdInt, out var msg)) {
+                        ctx.Queue(msg);
+                    }
+                } catch (InvalidOperationException) {
+                    // Invalid msgType, ignore
+                }
+            };
+            while (true) {
+                received.Reset();
+                ctx.Queue(new Message {
+                    id = -1, // Heartbeat does not need ID
+                    message = new HeartBeatMessage(lastMsgId.Value),
+                });
+                int index = WaitHandle.WaitAny(
+                    new WaitHandle[] { received, ctx.cts.Token.WaitHandle },
+                    TimeSpan.FromSeconds(10));
+                if (index == 0) {
+                    // Received a message from client
+                    continue;
+                } else if (index == 1) {
+                    // Closed connection
+                    break;
+                } else {
+                    // No response from client, close connection
+                    ctx.cts.Cancel();
+                    break;
+                }
+            }
+        }
 
         /// <summary>
         /// Closes the connection. <br/>No more web sockets or messages are accepted,
@@ -190,16 +155,12 @@ namespace RabiRiichi.Server.Core {
             if (isClosed.Exchange(true)) {
                 return;
             }
-            msgQueue?.CompleteAdding();
+            currentCtx?.Close();
         }
 
         protected void Dispose(bool disposing) {
             if (disposing) {
-                var cts = Interlocked.Exchange(ref sendCts, null);
-                if (cts != null) {
-                    cts.Cancel();
-                    cts.Dispose();
-                }
+                SwitchWSContext(null);
             }
         }
 
---
+++ RabiRiichi.Server/Core/RabiWSContext.cs
@@ -0,0 +1,128 @@
+using RabiRiichi.Communication.Json;
+using System.Collections.Concurrent;
+using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
+
+namespace RabiRiichi.Server.Core {
+    public class RabiWSContext {
+        /// <summary>
+        /// Websocket instance
+        /// </summary>
+        public readonly WebSocket ws;
+        /// <summary>
+        /// Player ID for this websocket
+        /// </summary>
+        public readonly int playerId;
+        /// <summary>
+        /// Message queue
+        /// </summary>
+        private readonly BlockingCollection<Connection.Message> msgQueue = new();
+        /// <summary>
+        /// Cancellation token source for all event loops related to this websocket.
+        /// </summary>
+        public readonly CancellationTokenSource cts = new();
+        /// <summary>
+        /// Callback when a message is received.
+        /// </summary>
+        public Action<JsonElement> OnReceive;
+        /// <summary>
+        /// Callback when the player is disconnected.
+        /// </summary>
+        public System.Action OnDisconnect;
+
+        public RabiWSContext(WebSocket ws, int playerId) {
+            this.ws = ws;
+            this.playerId = playerId;
+        }
+
+        /// <summary>
+        /// Run all loops related to this websocket.<br/>
+        /// Must call this method before using any callbacks.
+        /// </summary>
+        public async Task RunLoops(params Task[] extraTasks) {
+            Task.WaitAll(
+                extraTasks.Append(SendMsgLoop()).Append(ReceiveMsgLoop()).ToArray()
+            );
+            // Close connection.
+            await ws.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+            // Invoke callback. This will be called once and only once.
+            OnDisconnect?.Invoke();
+            // Dispose cancellation token source.
+            cts.Dispose();
+        }
+
+        /// <summary>
+        /// Queue a message to be sent.
+        /// </summary>
+        internal void Queue(Connection.Message msg) {
+            msg.TryQueue(msgQueue);
+        }
+
+        /// <summary>
+        /// Notify that no more message will be sent.
+        /// </summary>
+        internal void Close() {
+            msgQueue.CompleteAdding();
+        }
+
+        private async Task SendMsgLoop() {
+            while (true) {
+                try {
+                    var msg = msgQueue.Take(cts.Token);
+                    msg.isQueued.Set(false);
+                    var jsonStr = RabiJson.Stringify(msg, playerId);
+                    await ws.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(jsonStr)),
+                        WebSocketMessageType.Text, true, cts.Token);
+                } catch (OperationCanceledException) {
+                    // Cancellation token cancelled. Could be:
+                    // 1. A new ws connection coming in
+                    // 2. Client informed that they closed the connection
+                    break;
+                } catch (InvalidOperationException) {
+                    // Cannot take from queue anymore.
+                    // The upper layer has specified that no more message is being sent,
+                    // so we can safely close the connection. No further connection will be made.
+                    break;
+                }
+            }
+            // Notify other loops to stop
+            cts.Cancel();
+        }
+
+        private async Task ReceiveMsgLoop() {
+            var byteArr = new ArraySegment<byte>(new byte[1024]);
+            while (true) {
+                try {
+                    var msg = await ws.ReceiveAsync(byteArr, cts.Token);
+                    if (!msg.EndOfMessage) {
+                        // Too long, refuse to parse
+                        while (!(await ws.ReceiveAsync(byteArr, cts.Token)).EndOfMessage) { }
+                        continue;
+                    }
+                    if (msg.MessageType == WebSocketMessageType.Close) {
+                        // Client closes connection, notify send loop to close
+                        break;
+                    } else if (msg.MessageType == WebSocketMessageType.Text) {
+                        var jsonStr = Encoding.UTF8.GetString(byteArr.Array, 0, msg.Count);
+                        var json = JsonSerializer.Deserialize<JsonElement>(jsonStr);
+                        OnReceive?.Invoke(json);
+                    }
+                } catch (OperationCanceledException) {
+                    // Cancellation token cancelled. Could be:
+                    // 1. A new ws connection coming in
+                    // 2. Client informed that they closed the connection
+                    return;
+                } catch (ArgumentException) {
+                    // Invalid message
+                    continue;
+                } catch (JsonException) {
+                    // Invalid message
+                    continue;
+                }
+            }
+            // Notify other loops to stop
+            cts.Cancel();
+        }
+    }
+}
