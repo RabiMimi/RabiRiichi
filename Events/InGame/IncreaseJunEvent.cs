6fc4fe416b0d9075e9606e0996e4b681b735cd0d
KCFindstr <shimakaze@rabimimi.com>
2022-06-14 23:28:47 -0700

    Fix: Increased jun should be broadcasted

--- Events/InGame/IncreaseJunEvent.cs
+++ Events/InGame/IncreaseJunEvent.cs
@@ -1,9 +1,11 @@
+using RabiRiichi.Communication;
+
 namespace RabiRiichi.Events.InGame {
     public class IncreaseJunEvent : BroadcastPlayerEvent {
         public override string name => "increase_jun";
 
         #region Response
-        public int increasedJun;
+        [RabiBroadcast] public int increasedJun;
         #endregion
 
         public IncreaseJunEvent(EventBase parent, int playerId) : base(parent, playerId) { }
